adfceec17c69507cdd82906d63c0e8f4c83d5227
Rizki Arifudin <rizkiarifudin45@gmail.com>
2022-04-21 10:59:32 +0700

    fixing port to processEnv

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const errHandler = require("./helpers/errHandler");
 const router = require("./routes/index");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
